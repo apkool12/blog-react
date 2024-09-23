08de673f11f230c4b6f73f7632988c8fffb2e886
우은식 <apkool12@naver.com>
2024-09-24 00:34:36 +0900

    코딩애플 1-4 숙제

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = "강남 우동 맛집"
+  let [글제목1,b] = useState('남자 코트 추천')
+  let [글제목2,b2] = useState('강남 우동 맛집')
+  let [글제목3,b3] = useState('파이썬 독학')
 
   return (
     <div className="App">
       <div className="Black-nav">
         <h4>블로그</h4>
       </div>
-      <h4>{post}</h4>
+      <div className = "list">
+        <h4>{글제목1}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className = "list">
+        <h4>{글제목2}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className = "list">
+        <h4>{글제목3}</h4>
+        <p>2월 17일 발행</p>
+      </div>
 
     </div>
   );
