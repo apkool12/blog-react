d68e7669d8acf3839d2ad5605dec84432bc66609
우은식 <apkool12@naver.com>
2024-09-24 19:35:13 +0900

    코딩애플 1-5

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = "강남 우동 맛집"
-  let [글제목1,b] = useState('남자 코트 추천')
-  let [글제목2,b2] = useState('강남 우동 맛집')
-  let [글제목3,b3] = useState('파이썬 독학')
+  let post = "강남 우동 맛집";
+  let [글제목,b] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학']);
+  
+  let [좋아요,좋아요변경] = useState(0);
 
   return (
     <div className="App">
@@ -15,18 +15,19 @@ function App() {
         <h4>블로그</h4>
       </div>
       <div className = "list">
-        <h4>{글제목1} <span>👍</span> 0 </h4>
+        <h4>{글제목[0]} <span onClick={()=> {좋아요변경(좋아요+1)}}>👍</span> {좋아요} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className = "list">
-        <h4>{글제목2}</h4>
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className = "list">
-        <h4>{글제목3}</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
 
+      <button onClick={() => {b(['여자 코트 추천','강남 우동맛집','파이썬 독학'])}}> 버튼 </button>
 
     </div>
   );
